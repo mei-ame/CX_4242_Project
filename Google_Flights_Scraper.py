b2a9de67aa2363d22c90a0d93032aab9456ae976
Christine Ling <lingzijin6@gmail.com>
2024-10-28 14:19:36 -0400

    Fixed returning None issue in google flights scraper

--- Google_Flights_Scraper.py
+++ Google_Flights_Scraper.py
@@ -1,3 +1,5 @@
+from serpapi import GoogleSearch
+
 # Your SerpAPI API key
 API_KEY = "b637153e8613b18fc81533dfbf72045c9b43cbdd25323736bc3009ee6c38435a"
 
@@ -20,3 +22,44 @@ def search_flights(departure_id, arrival_id, outbound_date, return_date, currenc
         params["min_time"] = min_time
     if max_time:
         params["max_time"] = max_time
+
+    try:
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        
+        # Parse the best flights data
+        if "best_flights" in results:
+            flight_details = []
+            for flight in results["best_flights"]:
+                flight_info = {
+                    "total_duration": flight.get("total_duration"),
+                    "price": flight.get("price"),
+                    "type": flight.get("type"),
+                    "airline_logo": flight.get("airline_logo"),
+                    "layovers": [
+                        {
+                            "name": layover["name"],
+                            "duration": layover["duration"]
+                        } for layover in flight.get("layovers", [])
+                    ],
+                    "flights": [
+                        {
+                            "departure_airport": flight_leg["departure_airport"]["name"],
+                            "departure_time": flight_leg["departure_airport"]["time"],
+                            "arrival_airport": flight_leg["arrival_airport"]["name"],
+                            "arrival_time": flight_leg["arrival_airport"]["time"],
+                            "airline": flight_leg["airline"],
+                            "flight_number": flight_leg["flight_number"],
+                            "duration": flight_leg["duration"],
+                            "airplane": flight_leg["airplane"],
+                            "travel_class": flight_leg.get("travel_class"),
+                            "legroom": flight_leg.get("legroom"),
+                        } for flight_leg in flight.get("flights", [])
+                    ],
+                }
+                flight_details.append(flight_info)
+            return flight_details
+        else:
+            return "No flights found."
+    except Exception as e:
+        return str(e)
